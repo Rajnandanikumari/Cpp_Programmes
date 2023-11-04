6d065c15dfa40cd633aac5e9bd778b5c65fdab42
Codeco <118913721+Rajnandanikumari@users.noreply.github.com>
2023-11-04 22:56:34 +0530

    Add files via upload

---
+++ primenofun.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+ int nub()
+{
+    int i,n;
+    for(i=2;i<n-1;i++)
+        {
+            if(i%2==0)
+                cout<<"not prime";
+        }
+        else{
+            cout<<"prime";
+        }
+return 0;
+}
+int main()
+{
+    using namespace std;
+    cout<<nub();
+    return 0;
+}
