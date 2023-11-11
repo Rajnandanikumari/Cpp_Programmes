cae56694033614177be1050ae86ebf15d1c79692
Codeco <118913721+Rajnandanikumari@users.noreply.github.com>
2023-11-11 23:43:48 +0530

    Add files via upload

---
+++ pattfunn.cpp
@@ -0,0 +1,33 @@
+
+#include<iostream>
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    using namespace std;
+    int n =4,c=1;
+
+    for(int i=1;i<n;i++){
+        // space
+        for(int j=1;j<n;j++){
+           // cout<<"*";
+        {
+        // star
+        if((3-i<=j){
+        cout<<"*";
+        if(j<=3)
+        c++;
+        else
+        c--;
+        else
+        {
+            cout<<" ";
+        }
+        cout<<"*";
+        }
+        cout<<"\n";
+        cout<<endl;
+    }
+    
+    return 0;
+}
